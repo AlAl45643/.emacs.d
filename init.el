0fbd2ca7a38b5bae165fd8d1012d17f51d2d9c96
AlAl <alalwork@protonmail.com>
2026-06-08 19:42:47 -0400

    Add embark

--- init.el
+++ init.el
@@ -168,6 +168,8 @@
   "&" 'async-shell-command
   "!" 'compile
   "w" 'window-toggle-side-windows
+  "." 'embark-act
+  ";" 'embark-dwim
   )
 
 ;;;; my-second-leader-evil-map
@@ -1964,6 +1966,45 @@ changes."
    password-cache-expiry 3600)
   :config
   (require 'em-tramp))
+
+;;; embark
+;;;; packages
+(straight-use-package 'embark)
+(straight-use-package 'embark-consult)
+;;;; config
+(use-package embark
+  :general
+  (help-map
+   ;; ("C-." . embark-act)         ;; pick some comfortable binding
+   ;; ("C-;" . embark-dwim)        ;; good alternative: M-.
+   "B" 'embark-bindings) ;; alternative for `describe-bindings'
+
+  :init
+
+  ;; Optionally replace the key help with a completing-read interface
+  (setq prefix-help-command #'embark-prefix-help-command)
+
+  ;; Show the Embark target at point via Eldoc. You may adjust the
+  ;; Eldoc strategy, if you want to see the documentation from
+  ;; multiple providers. Beware that using this can be a little
+  ;; jarring since the message shown in the minibuffer can be more
+  ;; than one line, causing the modeline to move up and down:
+
+  ;; (add-hook 'eldoc-documentation-functions #'embark-eldoc-first-target)
+  ;; (setq eldoc-documentation-strategy #'eldoc-documentation-compose-eagerly)
+
+  ;; Add Embark to the mouse context menu. Also enable `context-menu-mode'.
+  ;; (context-menu-mode 1)
+  ;; (add-hook 'context-menu-functions #'embark-context-menu 100)
+
+  :config
+  ;; Hide the mode line of the Embark live/completions buffers
+  (add-to-list 'display-buffer-alist
+               '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
+                 nil
+                 (window-parameters (mode-line-format . none)))))
+
+;; Consult users will also want the embark-consult package.
 
 ;;; emacs
 (defun my-ctrl-g ()
