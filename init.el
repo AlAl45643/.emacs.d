7a7adcc02f15279dfce0c07c40c4c1644dcd4d9a
Ali Ali <AlAlwork@protonmail.com>
2026-09-14 20:41:25 -0400

    Format javascript through mode not lsp server

--- init.el
+++ init.el
@@ -433,7 +433,12 @@
 (defun my-format-buffer ()
   (interactive)
   (cond
-   ((and (featurep 'eglot) eglot--managed-mode (eglot-server-capable :documentFormattingProvider))
+   ((and (featurep 'eglot) eglot--managed-mode (eglot-server-capable :documentFormattingProvider) (not
+                                                                                                   (or (equal major-mode 'js-ts-mode)
+                                                                                                       (equal major-mode 'typescript-ts-mode)
+                                                                                                       (equal major-mode 'tsx-ts-mode)
+                                                                                                       (equal major-mode 'json-ts-mode)
+                                                                                                       (equal major-mode 'prisma-ts-mode))))
     (call-interactively #'eglot-format-buffer))
    (t (indent-region (point-min) (point-max)))))
 
